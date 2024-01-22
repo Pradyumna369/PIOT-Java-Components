3905a70fbcc4ab6eec0209a45df1c98545086f47
Pradyumna36 <nunna.pr@northeastern.edu>
2024-01-21 19:07:03 -0500

    lab-module-2-java

--- src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
+++ src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
@@ -13,6 +13,7 @@ package programmingtheiot.gda.app;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import programmingtheiot.gda.system.SystemPerformanceManager;
 
 /**
  * Main GDA application.
@@ -37,13 +38,16 @@ public class GatewayDeviceApp
 	 * 
 	 * @param args
 	 */
+	private SystemPerformanceManager sysPerfMgr = null;
 	public GatewayDeviceApp(String[] args)
 	{
 		super();
 		
 		_Logger.info("Initializing GDA...");
+		this.parseArgs(args);
 		
-		parseArgs(args);
+		this.sysPerfMgr = new SystemPerformanceManager();
+		
 	}
 	
 	
@@ -61,7 +65,7 @@ public class GatewayDeviceApp
 		gwApp.startApp();
 		
 		try {
-			Thread.sleep(DEFAULT_TEST_RUNTIME);
+			Thread.sleep(65000L);
 		} catch (InterruptedException e) {
 			// ignore
 		}
@@ -81,9 +85,13 @@ public class GatewayDeviceApp
 		_Logger.info("Starting GDA...");
 		
 		try {
-			// TODO: Your code here
-			
-			_Logger.info("GDA started successfully.");
+			if (this.sysPerfMgr.startManager()) {
+				_Logger.info("GDA started successfully.");
+			} else {
+				_Logger.warning("Failed to start system performance manager!");
+				
+				stopApp(-1);
+			}
 		} catch (Exception e) {
 			_Logger.log(Level.SEVERE, "Failed to start GDA. Exiting.", e);
 			
@@ -101,9 +109,11 @@ public class GatewayDeviceApp
 		_Logger.info("Stopping GDA...");
 		
 		try {
-			// TODO: Your code here
-			
-			_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
+			if (this.sysPerfMgr.stopManager()) {
+				_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
+			} else {
+				_Logger.warning("Failed to stop system performance manager!");
+			}
 		} catch (Exception e) {
 			_Logger.log(Level.SEVERE, "Failed to cleanly stop GDA. Exiting.", e);
 		}
